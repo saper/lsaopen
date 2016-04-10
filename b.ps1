8d7745313c5b25dd6eff10e3b9c9af13716be110
saper <saper@saper.info>
2016-04-10 03:26:54 +0200

    Update for NASM

--- b.ps1
+++ b.ps1
@@ -1,5 +1,2 @@
 nasm -f win32 lsaopen.asm
 c:\sw\asm\golink /console /debug=coff lsaopen.obj kernel32.dll advapi32.dll
-# c:\sw\asm\golink /console lsaopen.obj kernel32.dll advapi32.dll
-# c:\windbg\ntsd.exe lsaopen.exe
-
--- bn.ps1
+++ bn.ps1
@@ -1,3 +1,2 @@
-c:\sw\asm\goasm /l=lsaopen.lst lsaopen.asm
+nasm -t win32 lsaopen.asm
 c:\sw\asm\golink /console lsaopen.obj kernel32.dll advapi32.dll
-# c:\windbg\ntsd lsaopen.exe
